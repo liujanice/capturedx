d47b9e5e714d7c9197857ca37222352ee5a94a87
liujanice <janiceliu.2019@gmail.com>
2024-02-03 10:56:38 -0800

    wip

---
+++ CaptureDx/CaptureDx/CaptureDxApp.swift
@@ -0,0 +1,20 @@
+//
+//  CaptureDxApp.swift
+//  CaptureDx
+//
+//  Created by Janice Liu on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureDxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CaptureDx/CaptureDx/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CaptureDx
+//
+//  Created by Janice Liu on 2/2/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
